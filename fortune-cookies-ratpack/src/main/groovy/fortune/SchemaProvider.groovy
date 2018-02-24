ec5f915158212cbf056441902b365d64ad1e040c
Mario Garcia <mario.ggar@gmail.com>
2018-02-24 13:10:47 +0100

    Add database example to Ratpack's example

--- fortune-cookies-ratpack/src/main/groovy/fortune/SchemaProvider.groovy
+++ fortune-cookies-ratpack/src/main/groovy/fortune/SchemaProvider.groovy
@@ -1,3 +1,36 @@
+// tag::sqlInjected[]
+package fortune
+
+import javax.inject.Inject
+import javax.inject.Provider
+import groovy.sql.Sql
+import gql.DSL
+import graphql.schema.GraphQLSchema
+
+class SchemaProvider implements Provider<GraphQLSchema> {
+
+  @Inject
+  Sql sql // <1>
+
+  @Override
+  GraphQLSchema get() {
+    return DSL.mergeSchemas {
+      byResource('schema/Cookie.graphql')
+      byResource('schema/Schema.graphql') {
+        mapType('Queries') {
+           link('randomCookie') { env ->
+              String query = "SELECT * FROM cookies ORDER BY ID DESC LIMIT 1"
+
+              sql.firstRow query// <2>
+            }
+        }
+      }
+    }
+  }
+}
+// end::sqlInjected[]
+
+/**
 // tag::dummy[]
 package fortune
 
@@ -22,7 +55,7 @@ class SchemaProvider implements Provider<GraphQLSchema> {
   }
 }
 // end::dummy[]
-
+**/
 /**
 // tag::initial[]
 package fortune
--- fortune-cookies-ratpack/src/ratpack/Ratpack.groovy
+++ fortune-cookies-ratpack/src/ratpack/Ratpack.groovy
@@ -1,3 +1,103 @@
+// tag::withData[]
+import static ratpack.groovy.Groovy.ratpack
+
+import gql.ratpack.GraphQLModule
+import gql.ratpack.GraphQLHandler
+import gql.ratpack.GraphiQLHandler
+
+import graphql.schema.GraphQLSchema
+import fortune.SchemaProvider
+
+import ratpack.server.Service
+import ratpack.server.StartEvent
+import ratpack.groovy.sql.SqlModule
+import org.h2.jdbcx.JdbcDataSource
+import javax.sql.DataSource
+import groovy.sql.Sql
+
+ratpack {
+  bindings {
+        module GraphQLModule
+        module SqlModule
+        providerType GraphQLSchema, SchemaProvider
+        bindInstance DataSource, new JdbcDataSource(
+          URL: "jdbc:h2:mem::test;DB_CLOSE_DELAY=-1",
+          user: "sa",
+          password: ""
+        )
+        bindInstance new Service() { // <1>
+          void onStart(StartEvent e) {
+            Sql sql = e.registry.get(Sql) // <2>
+
+            sql.execute '''
+            CREATE TABLE cookies  (
+              ID INT PRIMARY KEY AUTO_INCREMENT,
+              AUTHOR VARCHAR(255),
+              TEXT VARCHAR(500)
+            )
+            '''
+
+            sql.execute '''
+            INSERT INTO cookies
+              (AUTHOR, TEXT)
+            VALUES
+              ('Anonymous', 'Dont talk to strangers')
+            '''
+
+            sql.execute '''
+            INSERT INTO cookies
+              (AUTHOR, TEXT)
+            VALUES
+              ('Anonymous', 'The greatest risk is not taking one')
+            '''
+          }
+        }
+  }
+
+  handlers {
+    post('graphql', GraphQLHandler)
+    get('graphql/browser', GraphiQLHandler)
+  }
+}
+// end::withData[]
+
+/**
+// tag::embeddedDataSource[]
+import static ratpack.groovy.Groovy.ratpack
+
+import gql.ratpack.GraphQLModule
+import gql.ratpack.GraphQLHandler
+import gql.ratpack.GraphiQLHandler
+
+import graphql.schema.GraphQLSchema
+import fortune.SchemaProvider
+
+import ratpack.groovy.sql.SqlModule
+import org.h2.jdbcx.JdbcDataSource
+import javax.sql.DataSource
+
+ratpack {
+  bindings {
+        module GraphQLModule
+        module SqlModule // <1>
+        providerType GraphQLSchema, SchemaProvider
+        bindInstance DataSource, new JdbcDataSource( // <2>
+            URL: "jdbc:h2:mem::test;DB_CLOSE_DELAY=-1",
+            user: "sa",
+            password: ""
+        )
+  }
+
+  handlers {
+    post('graphql', GraphQLHandler)
+    get('graphql/browser', GraphiQLHandler)
+  }
+}
+// end::embeddedDataSource[]
+**/
+
+/**
+// tag::initial[]
 import static ratpack.groovy.Groovy.ratpack
 
 import gql.ratpack.GraphQLModule
@@ -18,3 +118,5 @@ ratpack {
     get('graphql/browser', GraphiQLHandler) // <3>
   }
 }
+// end::initial[]
+**/
