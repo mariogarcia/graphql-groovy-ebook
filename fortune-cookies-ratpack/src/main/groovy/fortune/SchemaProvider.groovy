441bf4a3a81d6f085f4ef933d1660148bf9d1ec7
Mario Garcia <mario.ggar@gmail.com>
2018-02-24 11:00:51 +0100

    Add fortune cookie project structure and GraphiQL 1o1

--- fortune-cookies-ratpack/src/main/groovy/fortune/SchemaProvider.groovy
+++ fortune-cookies-ratpack/src/main/groovy/fortune/SchemaProvider.groovy
@@ -1,3 +1,30 @@
+// tag::dummy[]
+package fortune
+
+import javax.inject.Provider
+import gql.DSL
+import graphql.schema.GraphQLSchema
+
+class SchemaProvider implements Provider<GraphQLSchema> {
+
+  @Override
+  GraphQLSchema get() {
+    return DSL.mergeSchemas {
+      byResource('schema/Cookie.graphql')
+      byResource('schema/Schema.graphql') {
+        mapType('Queries') { // <1>
+            link('randomCookie') { env -> // <2>
+                return [author: 'Anonymous', text: "Don't talk to strangers"] // <3>
+            }
+        }
+      }
+    }
+  }
+}
+// end::dummy[]
+
+/**
+// tag::initial[]
 package fortune
 
 import javax.inject.Provider
@@ -14,3 +41,5 @@ class SchemaProvider implements Provider<GraphQLSchema> {
     }
   }
 }
+// end::initial[]
+**/
